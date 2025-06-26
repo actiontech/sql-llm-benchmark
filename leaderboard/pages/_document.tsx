b6eeb6d5fe4e74aa2f24a169ef9dcb3c83d3f66c
wanghucheng <eternity5701@163.com>
2025-06-26 14:20:30 +0800

    add: add google ga code

--- leaderboard/pages/_document.tsx
+++ leaderboard/pages/_document.tsx
@@ -64,6 +64,17 @@ class MyDocument extends Document<MyDocumentProps> {
     return (
       <Html lang={locale}>
         <Head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KPSPKB26');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=1500px, initial-scale=1" />
           <meta name="author" content="ActionTech" />
@@ -82,6 +93,14 @@ class MyDocument extends Document<MyDocumentProps> {
           />
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPSPKB26"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
